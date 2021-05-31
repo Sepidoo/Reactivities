7cc73083dec29b4ce9c0a3b30da9b9867685d9e6
Sepideh Jafari <jafari.sepid@gmail.com>
2021-05-31 16:00:09 +1000

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using API.Extensions;
+
 
 namespace API
 {
@@ -32,22 +30,8 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors( option =>
-             {
-                 option.AddPolicy("CorsPolicy" , policy=>{
-                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-                     });
-             }
-             );
+            services.AddApplicationServices(_config);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Application/Activities/Update.cs
@@ -0,0 +1,44 @@
+using System;
+using MediatR;
+using Domain;
+using Persistence;
+using System.Threading.Tasks;
+using System.Threading;
+using AutoMapper;
+
+namespace Application.Activities
+{
+    public class Update
+    {
+        public class Command : IRequest
+        {
+            public Command()
+            {
+            }
+
+
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                _mapper.Map(request.Activity,activity);
+                 await _context.SaveChangesAsync();
+                 return Unit.Value;
+            }
+        }
+    }
+
+
+}
